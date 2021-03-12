72c7a87d7ad43d4233f0625cf1a4bf6fa8ebd208
Jon Roberts <kewlguy@gmail.com>
2021-03-11 19:55:55 -0700

    start working on URI, got color in the uri search. however it set path to recordszzzz... hmmm

--- api/managed-records.js
+++ api/managed-records.js
@@ -12,10 +12,26 @@ var primaryColors = ["red", "blue", "yellow"]
 // Retrieve the following option, had to define as object in case if empty, it doesnt marked as "undefined"
 const retrieve = (options={}) => {
 //Run test will reveal its console log options (NOTE IF UNCOMMENTED WILL FAIL SPY LOG TEST)
-    console.log("Option detected:", options)
+
 
 // Variable that needed to be declared such as limit
     var limit = 10
+    console.log("Option detected:", options)
+
+//URI
+var uri = URI(window.path)
+    .addSearch("limit", limit+1)
+    // add color if exist
+    if(options.colors && options.colors.length > 0) {
+        uri.addSearch("color[]", options.colors);
+    }
+    //TODO .addSearch for offset (remember 10 per page)
+
+console.log(uri)
+
+
+    //FETCH
+
     
 
 return
